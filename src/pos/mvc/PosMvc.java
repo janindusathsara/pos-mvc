671321f3d2332c8bdc5a0696f1f0d3eb5658f0e8
janindusathsara <janindusathsara1111@gmail.com>
2023-07-30 08:30:25 +0530

    first commit

---
+++ src/pos/mvc/PosMvc.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pos.mvc;
+
+/**
+ *
+ * @author DELL i5
+ */
+public class PosMvc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");// TODO code application logic here
+    }
+    
+}
